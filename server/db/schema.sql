5d7aae3cbed6c7846f008897be3453716d774b5c
hiroki minagawa <hiroki6805@gmail.com>
2019-03-09 01:11:06 +0900

    usersテーブル作成

---
+++ server/db/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS `portfoliosns`;
+CREATE DATABASE `portfoliosns` DEFAULT CHARACTER SET utf8mb4;
+
+USE `portfoliosns`;
+
+CREATE TABLE `users` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` TEXT,
+  `auth_uid` VARCHAR(100) UNIQUE,
+  `self_introduction` TEXT,
+  `programming_experience` BIGINT,
+  `github_account` TEXT,
+  `twitter_account` TEXT,
+  `other_url` TEXT,
+  `other_url_comment` TEXT,
+  `created_by` BIGINT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` BIGINT,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`created_by`) REFERENCES users(`id`),
+  FOREIGN KEY (`updated_by`) REFERENCES users(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
