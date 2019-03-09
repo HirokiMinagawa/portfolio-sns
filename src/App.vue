59ccff8f6ad1356d2495cbfb55e6c7591cac1dc3
hiroki minagawa <hiroki6805@gmail.com>
2019-03-09 16:29:48 +0900

    トップページにログイン機能を実装

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,29 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span class="font-weight-light">Look My Portfolio</span>
+        <router-link class="toolbar-title font-weight-light" to="/">
+          <span>Look My Portfolio</span>
+        </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat href="#" target="_blank">
-        <span class="mr-2">マイアカウント</span>
-      </v-btn>
+      <v-toolbar-items>
+        <p v-if="user">{{ "こんにちは、" + user.displayName + "さん" }}</p>
+        <v-btn flat v-if="user">マイページ</v-btn>
+        <v-btn flat v-if="user" @click="signOut">ログアウト</v-btn>
+        <v-btn v-else flat @click.stop="dialog = true">
+          <span>新規登録 or ログイン</span>
+        </v-btn>
+        <v-dialog v-model="dialog" max-width="290px">
+          <v-card>
+            <v-container>
+              <v-layout justify-center>
+                <Auth @signInComplete="signInComplete"></Auth>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-dialog>
+      </v-toolbar-items>
     </v-toolbar>
-
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -17,15 +32,47 @@
 </template>
 
 <script>
+import Auth from "@/components/Auth";
+import firebase from "@/lib/firebase";
+
 export default {
   name: "App",
   components: {
-    //
+    Auth
   },
   data() {
     return {
-      //
+      dialog: false,
+      user: null
     };
+  },
+  created() {
+    this.getLoginUser();
+  },
+  methods: {
+    signInComplete: function() {
+      this.dialog = false;
+      this.user = firebase.auth().currentUser;
+      //ここでuser情報をDBに書き込みたい
+    },
+    signOut() {
+      firebase.auth().signOut();
+    },
+    getLoginUser: function() {
+      firebase.auth().onAuthStateChanged(user => {
+        this.user = user;
+      });
+    }
   }
 };
 </script>
+
+<style scoped>
+p {
+  margin: auto 30px;
+}
+.toolbar-title {
+  text-decoration: none;
+  color: black;
+}
+</style>
