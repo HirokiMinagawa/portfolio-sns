5505f23ee4f05bf9f921efbe97660d7bfd1aefa6
hiroki minagawa <hiroki6805@gmail.com>
2019-03-23 08:34:01 +0900

    index.js修正

--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "ページが見つかりません。" });
 });
 
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "エラーが起こりました。" });
