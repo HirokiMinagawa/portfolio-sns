ab7aedee787ae668d3a4c8446fd1092872e49ad5
hiroki minagawa <hiroki6805@gmail.com>
2019-03-22 23:55:17 +0900

    使用言語で表示するポートフォリオを絞る機能を追加

--- src/components/PortfolioCard.vue
+++ src/components/PortfolioCard.vue
@@ -1,7 +1,25 @@
 <template>
   <v-container fluid grid-list-xl>
+    <v-flex xs12 sm6 d-flex>
+      <v-select
+        @change="searchPortfolio"
+        v-model="selectedItem"
+        :items="programmingLanguageList"
+        box
+        label="使用言語"
+      >
+        <template v-slot:prepend-item>
+          <v-list-tile ripple @click="resetItems">
+            <v-list-tile-content>
+              <v-list-tile-title>条件なし</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider class="mt-2"></v-divider>
+        </template>
+      </v-select>
+    </v-flex>
     <v-layout row wrap justify-start>
-      <v-flex xs12 sm6 md4 lg3 v-for="portfolio in portfolios" :key="portfolio.id">
+      <v-flex xs12 sm6 md4 lg3 v-for="portfolio in filteredPortfolios" :key="portfolio.id">
         <v-card :to="{name: 'PortfolioInfo', params: { portfolioId: portfolio.id }}">
           <v-img :src="portfolio.thumbnail_url" height="200px"></v-img>
           <v-card-title primary-title>
@@ -30,21 +48,43 @@
 </template>
 
 <script>
-import { getAllPortfolioInfo } from "@/lib/api-service";
+import {
+  getAllPortfolioInfo,
+  getProgrammingLanguageList
+} from "@/lib/api-service";
 
 export default {
   data: function() {
     return {
-      portfolios: []
+      programmingLanguageList: [],
+      selectedItem: [],
+      portfolios: [],
+      filteredPortfolios: [],
     };
   },
   methods: {
     getPortfolioInfo: async function() {
       this.portfolios = await getAllPortfolioInfo();
+      this.filteredPortfolios = this.portfolios.slice();
+    },
+    getProgrammingLanguageList: async function() {
+      const res = await getProgrammingLanguageList();
+      this.programmingLanguageList = res.name;
+    },
+    searchPortfolio: function() {
+      if (!this.selectedItem) return null;
+      this.filteredPortfolios = this.portfolios.filter(portfolio => {
+        return portfolio.programmingLanguages.indexOf(this.selectedItem) >= 0;
+      });
+    },
+    resetItems: function() {
+      this.selectedItem = "";
+      this.filteredPortfolios = this.portfolios.slice();
     }
   },
   created() {
     this.getPortfolioInfo();
+    this.getProgrammingLanguageList();
   }
 };
 </script>
