b76b4c475a45f9ebd552ef6c91bd2a43232784df
hiroki minagawa <hiroki6805@gmail.com>
2019-03-23 10:16:24 +0900

    shema.js微修正

--- server/db/schema.js
+++ server/db/schema.js
@@ -7,9 +7,10 @@ const makeDb = async () => {
     return connection;
   };
   const connection = await getConnection();
-  await connection.query("DROP DATABASE IF EXISTS portfoliosns;");
-  await connection.query("CREATE DATABASE portfoliosns DEFAULT CHARACTER SET utf8mb4;");
-  await connection.query("USE portfoliosns;");
+//   await connection.query("DROP DATABASE IF EXISTS portfoliosns;");
+//   await connection.query("CREATE DATABASE portfoliosns DEFAULT CHARACTER SET utf8mb4;");
+//   await connection.query("USE portfoliosns;");
+  await connection.query("USE ld3e1ut8tgibw2l8;");
   await connection.query("CREATE TABLE users ( id BIGINT NOT NULL AUTO_INCREMENT,name TEXT NOT NULL,auth_uid VARCHAR(100) UNIQUE NOT NULL,prof_img_url TEXT,self_introduction TEXT,programming_experience FlOAT,github_account TEXT,twitter_account TEXT,other_url TEXT, other_url_comment TEXT,created_by BIGINT,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,updated_by BIGINT,updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id),FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,FOREIGN KEY (updated_by) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
   await connection.query("CREATE TABLE programming_languages (id BIGINT NOT NULL AUTO_INCREMENT,name TEXT,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
   await connection.query("INSERT INTO programming_languages (name) VALUES (?);",["C"]);
